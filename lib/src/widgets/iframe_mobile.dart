78e732bbea0251456302f272cbf1173dc9b93622
mark-hkz <mark-hkz>
2024-03-26 14:24:56 +0800

    Upgraded webview_flutter to v4.6.0 to support apple's 3rd party sdk requirements

--- lib/src/widgets/iframe_mobile.dart
+++ lib/src/widgets/iframe_mobile.dart
@@ -5,8 +5,9 @@ import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_html/src/navigation_delegate.dart';
 import 'package:flutter_html/src/replaced_element.dart';
 import 'package:flutter_html/style.dart';
-import 'package:webview_flutter/webview_flutter.dart' as webview;
 import 'package:html/dom.dart' as dom;
+import 'package:webview_flutter/webview_flutter.dart' as webview;
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 /// [IframeContentElement is a [ReplacedElement] with web content.
 class IframeContentElement extends ReplacedElement {
@@ -27,35 +28,100 @@ class IframeContentElement extends ReplacedElement {
 
   @override
   Widget toWidget(RenderContext context) {
-    final sandboxMode = attributes["sandbox"];
-    return Container(
+    return SizedBox(
       width: width ?? (height ?? 150) * 2,
       height: height ?? (width ?? 300) / 2,
       child: ContainerSpan(
         style: context.style,
         newContext: context,
-        child: webview.WebView(
-          initialUrl: src,
-          key: key,
-          javascriptMode: sandboxMode == null || sandboxMode == "allow-scripts"
-            ? webview.JavascriptMode.unrestricted
-            : webview.JavascriptMode.disabled,
-        navigationDelegate: (request) async {
-          final result = await navigationDelegate!(NavigationRequest(
-            url: request.url,
-            isForMainFrame: request.isForMainFrame,
-          ));
-          if (result == NavigationDecision.prevent) {
-            return webview.NavigationDecision.prevent;
-          } else {
-            return webview.NavigationDecision.navigate;
-          }
-        },
-          gestureRecognizers: {
-            Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer())
-          },
+        child: _WebViewWidget(
+          src: src,
+          navigationDelegate: navigationDelegate,
+          attributes: attributes,
         ),
       ),
+    );
+  }
+}
+
+class _WebViewWidget extends StatefulWidget {
+  const _WebViewWidget({
+    Key? key,
+    this.src,
+    required this.attributes,
+    required this.navigationDelegate,
+  }) : super(key: key);
+
+  final String? src;
+  final Map<String, String> attributes;
+  final NavigationDelegate? navigationDelegate;
+
+  @override
+  State<_WebViewWidget> createState() => _WebViewWidgetState();
+}
+
+class _WebViewWidgetState extends State<_WebViewWidget> {
+  late final webview.WebViewController controller;
+  late final webview.PlatformWebViewControllerCreationParams params;
+  final UniqueKey key = UniqueKey();
+
+  @override
+  void initState() {
+    super.initState();
+    if (webview.WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const webview.PlatformWebViewControllerCreationParams();
+    }
+
+    controller = webview.WebViewController.fromPlatformCreationParams(params);
+
+    final sandboxMode = widget.attributes["sandbox"];
+
+    controller
+        .setJavaScriptMode(
+      sandboxMode == null || sandboxMode == "allow-scripts"
+          ? webview.JavaScriptMode.unrestricted
+          : webview.JavaScriptMode.disabled,
+    )
+        .then((value) {
+      controller.setNavigationDelegate(
+        webview.NavigationDelegate(
+          onNavigationRequest: (request) async {
+            final result =
+                await widget.navigationDelegate?.call(NavigationRequest(
+              url: request.url,
+              isForMainFrame: request.isMainFrame,
+            ));
+            if (result == NavigationDecision.prevent) {
+              return webview.NavigationDecision.prevent;
+            } else {
+              return webview.NavigationDecision.navigate;
+            }
+          },
+        ),
+      ).then(
+        (value) {
+          if (widget.src != null) {
+            controller.loadRequest(Uri.parse(widget.src!));
+          }
+        },
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return webview.WebViewWidget(
+      controller: controller,
+      key: key,
+      gestureRecognizers: {
+        Factory<VerticalDragGestureRecognizer>(
+            () => VerticalDragGestureRecognizer())
+      },
     );
   }
 }
